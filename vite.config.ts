204fbe1bdff50100ff183529770de24cabbaa3df
TheYuvrajMishra <yuvraj17mishra11@gmail.com>
2025-05-02 14:05:32 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base:"/smooth-space-weaver",
+  base:"/smooth-space-weaver/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
